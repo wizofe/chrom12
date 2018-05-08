5ca2902c61d4cb9be5baea4c2a631978e8a383be
Abdulvahab <vahab_n@yahoo.com>
2018-05-09 00:45:09 +0100

    v1

--- frontlayer/src/cgi/result.py
+++ frontlayer/src/cgi/result.py
@@ -30,27 +30,45 @@ def result():
         else:
             result = 'Oops!! Result not found,refine your search and try again'
     return result
-html = "<html lang='en'>"
-html += '<head>'
-html += '<title> Result page </title>'
-#html += '<link rel ="stylesheet" type="text/css" href = "/Library/WebServer/Documents/chrom12/frontend/src/html/main.css">'
-html += "<style>"
-html += '''body{
-color:#660099;
-text-align: center;
-background-size:cover;
-background-color: rgba(255,255,255,0.7);
-background-blend-mode: lighten;
-background-image: url(/Library/Webserver/Documents/chrom12/frontend/src/img/chromo1.jpg);
-background-repeat: no-repeat;
-margin-bottom:50px;
-}'''
-html += "</style>"
-html += '</head>'
-html += '<body>'
-html += '<h4><b>Result as below</b></h4>'
-html += '<p>'+ result() + '</p>'
-html += '</body>'
-html += '</html>'
-print(html)
-def summary_result():
+def html():
+    html ='<html lang="en">'
+    html += '<head>'
+    html += "<title>Chromosome-12 Information</title>"
+    html += "<meta charset='utf-8'>"
+    html += "<meta name='viewport' content='width=device-width,initial-scale=1.0'>"
+    html += '<link rel ="stylesheet" type="text/css" href = "http://localhost/chrom12/frontlayer/src/html/main.css">'
+    #html += '<link rel="stylesheet" href="https://localhost/chrom12/frontlayer/src/html/main.css">'
+    html += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">'
+    html += '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>'
+    html += '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>'
+    html += "<style>"
+    html += '''body{
+    color:#660099;
+    text-align: center;
+    background-size:cover;
+    background-color: rgba(255,255,255,0.7);
+    background-blend-mode: lighten;
+    background-image: url('http://localhost/chrom12/frontlayer/src/img/chromo1.jpg');
+    background-repeat: no-repeat;
+    margin-bottom:50px;
+    }'''
+    html += "</style>"
+    html += '</head>'
+    html += "<div class='main-container'>"
+    html += "<header ><h1>Query Result</h1></header>"
+    html += '<nav>'
+    html += "<a href='http://localhost/chrom12/frontlayer/src/html/index.html'>Home  |</a>"
+    html += "<a href='http://localhost/cgi-bin/get_result.py'>Summary  |</a>"
+    html += "<a href='http://localhost/chrom12/frontlayer/src/html/contactus.html'>Contact us  </a>"
+    html += '</nav>'
+    html += "<div class='footer'>&copy; 2018,Group Chromosome-12.</div>"
+    html += "</style>"
+    html += '</head>'
+    html += '<body>'
+    html += '<h4><b>Result as below</b></h4>'
+    html += '<p>'+ result() + '</p>'
+    html += '</body>'
+    html += '</html>'
+    print(html)
+if __name__=="__main__":
+    html()
--- frontlayer/src/cgi/summary.py
+++ frontlayer/src/cgi/summary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
-=======
+
 """*************************************************************************************************
 * project: Genome browse for chromosome-12                                                         *
 * file:get_result.py                                                                               *
@@ -10,7 +9,7 @@
 * Licence:See the LICENSE.txt for licensing information                                            *
 * Author: Abdulvahab Kharadi                                                                       *
 * ***********************************************************************************************"""
->>>>>>> master
+
 from middlelayer import vari
 import json
 import cgi
@@ -24,11 +23,11 @@ def result():
 def html():
     html ='<html lang="en">'
     html += '<head>'
-    html += "<meta charset='utf-8'>"
     html += "<title>Chromosome-12 Information</title>"
-    html += '<link rel ="stylesheet" type="text/css" href = "http://localhost/chrom12/frontend/src/html/main.css">'
-    html += "<meta name='viewport' content='width=device-width,initial-scale=1'>"
-    html += '<link rel="stylesheet" href="https://localhost/chrom12/frontend/src/html/main.css">'
+    html += "<meta charset='utf-8'>"
+    html += "<meta name='viewport' content='width=device-width,initial-scale=1.0'>"
+    html += '<link rel ="stylesheet" type="text/css" href = "http://localhost/chrom12/frontlayer/src/html/main.css">'
+    #html += '<link rel="stylesheet" href="https://localhost/chrom12/frontlayer/src/html/main.css">'
     html += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">'
     html += '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>'
     html += '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>'
@@ -39,7 +38,7 @@ def html():
     background-size:cover;
     background-color: rgba(255,255,255,0.7);
     background-blend-mode: lighten;
-    background-image: url('http://localhost/chrom12/frontend/src/img/chromo1.jpg');
+    background-image: url('http://localhost/chrom12/frontlayer/src/img/chromo1.jpg');
     background-repeat: no-repeat;
     margin-bottom:50px;
     }'''
@@ -48,52 +47,32 @@ def html():
     html += "<div class='main-container'>"
     html += "<header ><h1>Summary of Chromosome 12</h1></header>"
     html += '<nav>'
-    html += "<a href='http://localhost/chrom12/frontend/src/html/index.html'>Home  |</a>"
-    html += "<a href='http://localhost/cgi-bin/get_result.py'>Summary  |</a>"
-    html += "<a href='http://localhost/chrom12/frontend/src/html/contactus.html'>Contact us  </a>"
+    html += "<a href='http://localhost/chrom12/frontlayer/src/html/index.html'>Home  |</a>"
+    html += "<a href='http://localhost/cgi-bin/summary.py'>Summary  |</a>"
+    html += "<a href='http://localhost/chrom12/frontlayer/src/html/contactus.html'>Contact us  </a>"
     html += '</nav>'
     html += "<div class='footer'>&copy; 2018,Group Chromosome-12.</div>"
     html += '<div>'
-    html += "<table class= 'table table-striped'>"
+    html += "<table class=table table-condensed >"
     html += "<thead>"
     html += "<tr>"
     html += "<th>Gene Identifires</th>"
     html += "<th>Genebank Accssion</th>"
-    html += '<th>Chromosomal Location</th>'
     html += '<th>Protein Product</th>'
+    html += '<th>Chromosomal Location</th>'
     html += '</tr>'
     html += '</thead>'
-    html += '<tbody style=text-align:center;>'
-    html += '<tr style=text-align:center;>'
-    html += "<th id='gene_id'>{}</th>".format(result()['gene_id'])
-    html += "<th id='acc_num'>{}</th>".format(result()['acc_num'])
-    html += "<th id='protein'>{}</th>".format(result()['protein'])
-    html += "<th id='chromo_location'>{}</th>".format(result()['chromo_location'])
-    html += '</tr>'
-    html += '<tr>'
-    html += "<th id='gene_id'>{}</th>".format(result()['gene_id'])
-    html += "<th id='acc_num'>{}</th>".format(result()['acc_num'])
-    html += "<th id='protein'>{}</th>".format(result()['protein'])
-    html += "<th id='chromo_location'>{}</th>".format(result()['chromo_location'])
-    html += '</tr>'
-    html += '<tr>'
-    html += "<th id='gene_id'>{}</th>".format(result()['gene_id'])
-    html += "<th id='acc_num'>{}</th>".format(result()['acc_num'])
-    html += "<th id='protein'>{}</th>".format(result()['protein'])
-    html += "<th id='chromo_location'>{}</th>".format(result()['chromo_location'])
-    html += '</tr>'
-    html += '<tr>'
-    html += "<th id='gene_id'>{}</th>".format(result()['gene_id'])
-    html += "<th id='acc_num'>{}</th>".format(result()['acc_num'])
-    html += "<th id='protein'><a href='http://localhost/chrom12/frontend/src/html/contactus.html'>{}</a ></th>".format(result()['protein'])
-    html += "<th id='chromo_location'>{}</th>".format(result()['chromo_location'])
-    html += '</tr>'
+    for i in range(len(result())):
+        html += '<tr style=text-align:center;>'
+        html += "<th id='gene_id'>{}</th>".format(result()['gene_id'][i])
+        html += "<th id='acc_num'>{}</th>".format(result()['acc_num'][i])
+        html += "<th id='protein'>{}</th>".format(result()['protein'][i])
+        html += "<th id='chromo_location'>{}</th>".format(result()['chromo_location'][i])
+        html += '</tr>'
     html += '</tbody>'
     html += '</table>'
     html += '<div>'
     html += '<script>'
-# += 'var result = JSON.parse({})'.format(result)
-#html +="document.getElementById(gene_id).innerHTML+= {}+=".format(result()['gene_id'])
     html += '</body>'
     html += '</html>'
     print(html)
